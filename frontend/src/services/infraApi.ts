05c0c86ecbb9abf46d1f4f3e95ddbf8abd828d9f
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2025-07-05 14:07:26 -0400

    finalizado

--- frontend/src/services/infraApi.ts
+++ frontend/src/services/infraApi.ts
@@ -6,6 +6,9 @@ const PORT:string = import.meta.env.VITE_API_INFRA_PORT;
 
 const baseUrl:string = `${URL}:${PORT}`;
 
+// Url caso o server seja DHCP 
+// const baseUrl:string =  `${window.location.protocol}//${window.location.hostname}:5433`;
+
 const infraApi = axios.create({
     baseURL: baseUrl
 });
--- frontend/src/services/switchApi.ts
+++ frontend/src/services/switchApi.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-const URL:string = import.meta.env.VITE_API_SW_URL;
-const PORT:string = import.meta.env.VITE_API_SW_PORT;
+// const URL:string = import.meta.env.VITE_API_SW_URL;
+// const PORT:string = import.meta.env.VITE_API_SW_PORT;
 
-const baseUrl:string = `${URL}:${PORT}`;
+// const baseUrl:string = `${URL}:${PORT}`;
+
+// url caso o server seja DHCP
+const baseUrl:string =  `${window.location.protocol}//${window.location.hostname}:5000`;
+// console.log("BASE URL: ", baseUrl)
 
 const switchApi = axios.create({
     baseURL: baseUrl
